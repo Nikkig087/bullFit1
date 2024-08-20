04a7d56f8a1ba2543466e7477da9dad9f184b0b3
Nicola Getsee <Nicola.getsee@gmail.com>
2024-08-20 20:25:19 +0000

    contact form and thank you message working

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import contact_view, thank_you_view
+from .views import contact_form
 
 urlpatterns = [
     path('', views.ExerciseListView.as_view(), name='home'),
@@ -8,6 +8,6 @@ urlpatterns = [
     path('exercise/<int:pk>/comment/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
     path('exercise/<int:pk>/comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('exercise/<int:pk>/add_comment/', views.add_comment, name='add_comment'),
-    path('contact/', contact_view, name='contact'),  # For function-based view
-    path('thank-you/', thank_you_view, name='thank_you'), 
+    path('contact/', contact_form, name='contact_form'),  # For function-based view
+    #path('thank-you/', thank_you_view, name='thank_you'), 
 ]
